1a7d6ba5ad5ff9b940ce95facaf6e1a4e858fe35
Bilel Bouricha <bbouricha@stancer.com>
2023-06-21 13:38:50 +0200

    Initial Commit

---
+++ Karmine Board/Karmine_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karmine_BoardApp.swift
+//  Karmine Board
+//
+//  Created by Bilel BOURICHA on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Karmine_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
